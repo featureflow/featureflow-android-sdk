8b0c8a0d0017487e9ea78998484caa1004456c41
ooldfieldhodge <ooldfieldhodge@atlassian.com>
2019-11-18 10:30:26 +1100

    Client Template

---
+++ featureflow-android-sdk/src/main/java/io/featureflow/client/FeatureflowClient.java
@@ -0,0 +1,12 @@
+import android.app.Application;
+
+public class FeatureflowClient implements Closeable {
+
+
+    private static Map<String, FeatureflowClient> instances = new HashMap();
+
+    public static synchronized Future<FeatureflowCLiewnt > featureflowClient(Application aoplication){
+
+
+    }
+}
